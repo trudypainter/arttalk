d30e84260d6e87556a159296f2374c9f678db176
Apstoddard <andrewpstoddard@gmail.com>
2023-05-06 13:49:28 -0400

    fixes ts

--- src/components/Heatmap.tsx
+++ src/components/Heatmap.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable prefer-const */
 import React, { useState, useEffect, useRef } from "react";
 import Sketch from "react-p5";
 import p5Types from "react-p5/node_modules/@types/p5"; // Update this import statement
